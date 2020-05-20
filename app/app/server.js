507ef6096fe9ad0849eced8ef4c90ab165b4a837
Alex Zerah <alex.zerah@gmail.com>
2020-05-20 13:26:03 +0200

    Fix bug
    - fix views folder

--- app/app/server.js
+++ app/app/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 1337
 const db = require('./db')
 
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.get('/', function (req, res) {
