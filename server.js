204fa91e8fb76f9ba50ca41dcfc188144f9d3416
Antoine Gouteix <antoine.gouteix@edu.devinci.fr>
2020-05-20 10:39:34 +0200

    Serveur web

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var app = express();
+var port = 1337
+
+
+/* Route */
+app.get("/", function(req, res) {
+    res.sendFile(__dirname+'/index.html')
+});
+
+/* Route /nice */
+app.get("/nice", function(req, res) {
+    res.send("Nice");
+});
+
+app.listen(port, function() {
+    console.log("Express Node.js le serveur troune sur le port 1337.");
+})
