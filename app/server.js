35dfd4f779ee5daf4f4f8ed3e8d16310487a2383
Alex Zerah <alex.zerah@gmail.com>
2020-05-20 12:50:04 +0200

    Add techs
    - nginx
    - docker

--- app/server.js
+++ app/server.js
@@ -4,15 +4,15 @@ const port = 1337
 
 app.set('view engine', 'pug');
 
+app.get('/', function (req, res) {
+    res.render('index', { title: 'Hey', message: 'Hello there!'});
+});
+
 /* Route /nice */
 app.get("/nice", function(req, res) {
     res.send("Nice");
 });
 
-app.get('/', function (req, res) {
-    res.render('index', { title: 'Hey', message: 'Hello there!'});
-});
-
 app.listen(port, function() {
-    console.log("Express Node.js le serveur tourne sur le port 1337.");
+    console.log("le serveur tourne sur le port 1337.");
 })
---
+++ node.dockerfile
@@ -0,0 +1,10 @@
+FROM node:lts-alpine
+
+WORKDIR /usr/src/app
+COPY app/package*.json ./
+
+RUN npm install
+COPY app .
+
+EXPOSE 1337
+CMD ["node", "server.js"]
